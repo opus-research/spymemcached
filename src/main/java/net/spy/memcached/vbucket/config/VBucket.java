c234bc0b22b34a71970d323014da09c47f224d02
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:26 +0000

    First commited as after_6671_rev7

---
+++ src/main/java/net/spy/memcached/vbucket/config/VBucket.java
@@ -0,0 +1,14 @@
+package net.spy.memcached.vbucket.config;
+
+public class VBucket {
+
+	public final static int MAX_REPLICAS = 4;
+
+	public final static int MAX_BUCKETS = 65536;
+
+	private final int[] servers = new int[MAX_REPLICAS + 1];
+
+	public int[] getServers() {
+		return servers;
+	}
+}

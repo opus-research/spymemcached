84c9f603c0553a7019ce38b743ad270f769f6014
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:32 +0000

    First commited as after_6778_rev3

--- src/main/java/net/spy/memcached/MemcachedClient.java
+++ src/main/java/net/spy/memcached/MemcachedClient.java
@@ -39,6 +39,7 @@ import net.spy.memcached.ops.CancelledOperationStatus;
 import net.spy.memcached.ops.ConcatenationType;
 import net.spy.memcached.ops.DeleteOperation;
 import net.spy.memcached.ops.GetOperation;
+import net.spy.memcached.ops.GetlOperation;
 import net.spy.memcached.ops.GetsOperation;
 import net.spy.memcached.ops.Mutator;
 import net.spy.memcached.ops.Operation;
@@ -986,6 +987,54 @@ public class MemcachedClient extends SpyThread
 	}
 
 	/**
+	 * Gets and locks the given key asynchronously.
+	 *
+	 * @param key the key to fetch and lock
+	 * @param exp the amount of time the lock should be valid for
+	 * @param tc the transcoder to serialize and unserialize value
+	 * @return a future that will hold the return value of the fetch
+	 * @throws IllegalStateException in the rare circumstance where queue
+	 *         is too full to accept any more requests
+	 */
+	public <T> Future<T> asyncGetAndLock(final String key, int exp, final Transcoder<T> tc) {
+
+		final CountDownLatch latch=new CountDownLatch(1);
+		final GetFuture<T> rv=new GetFuture<T>(latch, operationTimeout);
+
+		Operation op=opFact.getl(key, exp,
+				new GetlOperation.Callback() {
+			private Future<T> val=null;
+			public void receivedStatus(OperationStatus status) {
+				rv.set(val);
+			}
+			public void gotData(String k, int flags, byte[] data) {
+				assert key.equals(k) : "Wrong key returned";
+				val=tcService.decode(tc,
+						new CachedData(flags, data, tc.getMaxSize()));
+			}
+			public void complete() {
+				latch.countDown();
+			}});
+		rv.setOperation(op);
+		addOp(key, op);
+		return rv;
+	}
+
+	/**
+	 * Get and lock the given key asynchronously and decode with the default
+	 * transcoder.
+	 *
+	 * @param key the key to fetch and lock
+	 * @param exp the amount of time the lock should be valid for
+	 * @return a future that will hold the return value of the fetch
+	 * @throws IllegalStateException in the rare circumstance where queue
+	 *         is too full to accept any more requests
+	 */
+	public Future<Object> asyncGetAndLock(final String key, int exp) {
+		return asyncGetAndLock(key, exp, transcoder);
+	}
+
+	/**
 	 * Asynchronously get a bunch of objects from the cache.
 	 *
 	 * @param <T>
@@ -1315,6 +1364,46 @@ public class MemcachedClient extends SpyThread
 		}
 		getLogger().debug("Mutation returned %s", rv);
 		return rv.get();
+	}
+
+	/**
+	 * Getl with a single key.
+	 *
+	 * @param key the key to get and lock
+	 * @param exp the expiration time for the lock
+	 * @param tc the transcoder to serialize and unserialize value
+	 * @return the result from the cache (null if there is none)
+	 * @throws OperationTimeoutException if the global operation timeout is
+	 *		   exceeded
+	 * @throws IllegalStateException in the rare circumstance where queue
+	 *         is too full to accept any more requests
+	 */
+	public <T> T getAndLock(String key, int exp, Transcoder<T> tc) {
+		try {
+			return asyncGetAndLock(key, exp, tc).get(
+					operationTimeout, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			throw new RuntimeException("Interrupted waiting for value", e);
+		} catch (ExecutionException e) {
+			throw new RuntimeException("Exception waiting for value", e);
+		} catch (TimeoutException e) {
+			throw new OperationTimeoutException("Timeout waiting for value", e);
+		}
+	}
+
+	/**
+	 * Get and lock with a single key and decode using the default transcoder.
+	 *
+	 * @param key the key to get and lock
+	 * @param exp the expiration time for the lock
+	 * @return the result from the cache (null if there is none)
+	 * @throws OperationTimeoutException if the global operation timeout is
+	 *		   exceeded
+	 * @throws IllegalStateException in the rare circumstance where queue
+	 *         is too full to accept any more requests
+	 */
+	public Object getAndLock(String key, int exp) {
+		return getAndLock(key, exp, transcoder);
 	}
 
 	/**

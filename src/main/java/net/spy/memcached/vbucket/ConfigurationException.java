246cc814cb7a672cb20fc0d3c9306635d80e96f6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:02 +0000

    First commited as before_7778_rev1

--- src/main/java/net/spy/memcached/vbucket/ConfigurationException.java
+++ src/main/java/net/spy/memcached/vbucket/ConfigurationException.java
@@ -2,8 +2,6 @@ package net.spy.memcached.vbucket;
 
 public class ConfigurationException extends RuntimeException {
 
-    private static final long serialVersionUID = -9180877058910807939L;
-
     public ConfigurationException() {
         super();
     }

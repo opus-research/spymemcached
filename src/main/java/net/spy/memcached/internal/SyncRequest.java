2ab0c32c4cb873788a213335cb685b3c9c18e08f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:53 +0000

    First commited as after_7048_rev1

---
+++ src/main/java/net/spy/memcached/internal/SyncRequest.java
@@ -0,0 +1,29 @@
+package net.spy.memcached.internal;
+
+public class SyncRequest {
+	private final String key;
+	private final Long cas;
+	
+	public SyncRequest(String key) {
+		this.key = key;
+		this.cas = 0L;
+	}
+	
+	public SyncRequest(String key, Long cas) {
+		this.key = key;
+		this.cas =cas;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public Long getCas() {
+		return cas;
+	}
+	
+	@Override
+	public String toString() {
+		return "Key: " + key + ", Cas: " + cas;
+	}
+}

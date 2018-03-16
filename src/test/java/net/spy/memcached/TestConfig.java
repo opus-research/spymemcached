366e8118826ee167cb1ef74620ea9197dce1a2f3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:00 +0000

    First commited as after_7651_rev2

--- src/test/java/net/spy/memcached/TestConfig.java
+++ src/test/java/net/spy/memcached/TestConfig.java
@@ -3,9 +3,17 @@ package net.spy.memcached;
 public class TestConfig {
 	public static final String IPV4_PROP = "server.address_v4";
 	public static final String IPV6_PROP = "server.address_v6";
+	public static final String TYPE_MEMCACHED = "memcached";
+	public static final String TYPE_MEMBASE = "membase";
+	public static final String TYPE_COUCHBASE = "couchbase";
 
 	public static final String IPV4_ADDR = System.getProperty(IPV4_PROP, "127.0.0.1");
 	public static final String IPV6_ADDR = resolveIpv6Addr();
+	public static final String TYPE = System.getProperty("server.type", "memcached").toLowerCase();
+
+	private TestConfig(){
+		// Empty
+	}
 
 	private static String resolveIpv6Addr() {
 		String ipv6 = System.getProperty(IPV6_PROP, "::1");;
@@ -13,5 +21,17 @@ public class TestConfig {
 			return "::ffff:" + ipv6;
 		}
 		return ipv6;
+	}
+
+	public static final boolean isMemcached() {
+		return TYPE.equals(TYPE_MEMCACHED);
+	}
+
+	public static final boolean isMembase() {
+		return TYPE.equals(TYPE_MEMBASE);
+	}
+
+	public static final boolean isCouchbase() {
+		return TYPE.equals(TYPE_COUCHBASE);
 	}
 }

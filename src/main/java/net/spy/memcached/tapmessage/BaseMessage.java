8c21ce0469bcb0acb55066361d2a4a6677fea605
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:48:35 +0000

    First commited as before_9760_rev1

--- src/main/java/net/spy/memcached/tapmessage/BaseMessage.java
+++ src/main/java/net/spy/memcached/tapmessage/BaseMessage.java
@@ -1,3 +1,25 @@
+/**
+ * Copyright (C) 2009-2011 Couchbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
+ * IN THE SOFTWARE.
+ */
+
 package net.spy.memcached.tapmessage;
 
 import java.nio.ByteBuffer;
@@ -5,205 +27,303 @@ import java.nio.ByteBuffer;
 import net.spy.memcached.compat.SpyObject;
 
 /**
- * The HeaderMessage implements the header of a tap message. This class cannot be instantiated.
- * Tap stream messages are created with the RequestMessage and ResponseMessage classes. Users
- * who want to take advantage of customizing their own tap messages should use the
- * CustomTapStream class since it provides flexibility to create all valid tap messages.
+ * The HeaderMessage implements the header of a tap message. This class cannot
+ * be instantiated. Tap stream messages are created with the RequestMessage and
+ * ResponseMessage classes. Users who want to take advantage of customizing
+ * their own tap messages should use the CustomTapStream class since it provides
+ * flexibility to create all valid tap messages.
  */
-public abstract class BaseMessage extends SpyObject {
-	protected static final int MAGIC_OFFSET = 0;
-	protected static final int OPCODE_OFFSET = 1;
-	protected static final int KEYLENGTH_OFFSET = 2;
-	protected static final int EXTRALENGTH_OFFSET = 4;
-	protected static final int DATATYPE_OFFSET = 5;
-	protected static final int VBUCKET_OFFSET = 6;
-	protected static final int TOTALBODY_OFFSET = 8;
-	protected static final int OPAQUE_OFFSET = 12;
-	protected static final int CAS_OFFSET = 16;
-	public static final int HEADER_LENGTH = 24;
+public class BaseMessage extends SpyObject {
+  /**
+   * The index of the magic field in a tap header.
+   */
+  public static final int MAGIC_INDEX = 0;
 
-	protected TapMagic magic;
-	protected TapOpcode opcode;
-	protected short keylength;
-	protected byte extralength;
-	protected byte datatype;
-	protected short vbucket;
-	protected int totalbody;
-	protected int opaque;
-	protected long cas;
+  /**
+   * The length of the magic field in a tap header.
+   */
+  public static final int MAGIC_FIELD_LENGTH = 1;
 
-	/**
-	 * Sets the value of the tap messages magic field.
-	 * @param m The new value for the magic field.
-	 */
-	public final void setMagic(TapMagic m) {
-		magic = m;
-	}
+  /**
+   * The index of the opcode field in a tap header.
+   */
+  public static final int OPCODE_INDEX = 1;
 
-	/**
-	 * Gets the value of the tap messages magic field.
-	 * @return The value of the magic field.
-	 */
-	public final TapMagic getMagic() {
-		return magic;
-	}
+  /**
+   * The length of the opcode field in a tap header.
+   */
+  public static final int OPCODE_FIELD_LENGTH = 1;
 
-	/**
-	 * Sets the value of the tap messages opcode field
-	 * @param o The new value of the opcode field.
-	 */
-	public final void setOpcode(TapOpcode o) {
-		opcode = o;
-	}
+  /**
+   * The index of the key length field in a tap header.
+   */
+  public static final int KEY_LENGTH_INDEX = 2;
 
-	/**
-	 * Gets the value of the tap messages opaque field.
-	 * @return The value of the opaque field.
-	 */
-	public final TapOpcode getOpcode() {
-		return opcode;
-	}
+  /**
+   * The length of the key length field in a tap header.
+   */
+  public static final int KEY_LENGTH_FIELD_LENGTH = 2;
 
-	/**
-	 * Gets the value of the tap messages key length field.
-	 * @return The value of the key length field.
-	 */
-	public final short getKeylength() {
-		return keylength;
-	}
+  /**
+   * The index of the extra length field in the tap header.
+   */
+  public static final int EXTRA_LENGTH_INDEX = 4;
 
-	/**
-	 * Sets the value of the tap messages data type field.
-	 * @param b The new value for the data type field.
-	 */
-	public final void setDatatype(byte d) {
-		datatype = d;
-	}
+  /**
+   * The length of the extra length field in a tap header.
+   */
+  public static final int EXTRA_LENGTH_FIELD_LENGTH = 1;
 
-	/**
-	 * Gets the value of the tap messages data type field.
-	 * @return The value of the data type field.
-	 */
-	public final byte getDatatype() {
-		return datatype;
-	}
+  /**
+   * The index of the data type field in the tap header.
+   */
+  public static final int DATA_TYPE_INDEX = 5;
 
-	/**
-	 * Sets the value of the tap messages extra length field.
-	 * @param i The new value for the extra length field.
-	 */
-	public final void setExtralength(byte e) {
-		extralength = e;
-	}
+  /**
+   * The length of the data type field in a tap header.
+   */
+  public static final int DATA_TYPE_FIELD_LENGTH = 1;
 
-	/**
-	 * Gets the value of the tap messages extra length field.
-	 * @return The value of the extra length field.
-	 */
-	public final byte getExtralength() {
-		return extralength;
-	}
+  /**
+   * The index of the vbucket field in the tap header.
+   */
+  public static final int VBUCKET_INDEX = 6;
 
-	/**
-	 * Sets the value of the tap messages vbucket field.
-	 * @param vb The new value for the vbucket field.
-	 */
-	public final void setVbucket(short vb) {
-		vbucket = vb;
-	}
+  /**
+   * The length of the vbucket field in a tap header.
+   */
+  public static final int VBUCKET_FIELD_LENGTH = 2;
 
-	/**
-	 * Gets the value of the tap messages vbucket field.
-	 * @return The value of the vbucket field.
-	 */
-	public final short getVbucket() {
-		return vbucket;
-	}
+  /**
+   * The index of the total body field in the tap header.
+   */
+  public static final int TOTAL_BODY_INDEX = 8;
 
-	/**
-	 * Sets the value of the tap messages total body field.
-	 * @param l The new value for the total body field.
-	 */
-	public final void setTotalbody(int t) {
-		totalbody = t;
-	}
+  /**
+   * The length of the total body field in the tap header.
+   */
+  public static final int TOTAL_BODY_FIELD_LENGTH = 4;
 
-	/**
-	 * Gets the value of the tap messages total body field.
-	 * @return The value of the total body field.
-	 */
-	public final int getTotalbody() {
-		return totalbody;
-	}
+  /**
+   * The index of the opaque field in the tap header.
+   */
+  public static final int OPAQUE_INDEX = 12;
 
-	/**
-	 * Sets the value of the tap messages opaque field.
-	 * @param op The new value for the opaque field.
-	 */
-	public final void setOpaque(int op) {
-		opaque = op;
-	}
+  /**
+   * The length of the opaque field in the tap header.
+   */
+  public static final int OPAQUE_FIELD_LENGTH = 4;
 
-	/**
-	 * Gets the value of the tap messages opaque field.
-	 * @return The value of the opaque field.
-	 */
-	public final int getOpaque() {
-		return opaque;
-	}
+  /**
+   * The index of the cas field in the tap header.
+   */
+  public static final int CAS_INDEX = 16;
 
-	/**
-	 * Sets the value of the tap messages cas field.
-	 * @param cas The new value for the cas field.
-	 */
-	public final void setCas(long c) {
-		cas = c;
-	}
+  /**
+   * The length of the cas field in the tap header.
+   */
+  public static final int CAS_FIELD_LENGTH = 8;
 
-	/**
-	 * Gets the value of the tap messages cas field.
-	 * @return The value of the cas field.
-	 */
-	public final long getCas() {
-		return cas;
-	}
+  /**
+   * The header length.
+   */
+  public static final int HEADER_LENGTH = 24;
 
-	/**
-	 * Gets the length of the entire message.
-	 * @return The length of the message.
-	 */
-	public final int getMessageLength() {
-		return HEADER_LENGTH + getTotalbody();
-	}
+  /**
+   * Holds the binary data for the header field.
+   */
+  protected byte[] mbytes;
 
-	/**
-	 * Creates a ByteBuffer representation of the message.
-	 * @return The ByteBuffer representation of the message.
-	 */
-	public ByteBuffer getBytes() {
-		
-		return null;
-	}
+  /**
+   * Instantiates a tap header.
+   */
+  protected BaseMessage() {
+    mbytes = new byte[HEADER_LENGTH];
+  }
 
-	protected short decodeShort(byte[] data, int i) {
-		return (short)((data[i] & 0xff) << 8 | (data[i + 1] & 0xff));
-	}
+  /**
+   * Sets the value of the tap messages magic field.
+   *
+   * @param m The new value for the magic field.
+   */
+  public final void setMagic(TapMagic m) {
+    mbytes[MAGIC_INDEX] = m.getMagic();
+  }
 
-	protected int decodeInt(byte[] data, int i) {
-		return (data[i] & 0xff) << 24
-			| (data[i + 1] & 0xff) << 16
-			| (data[i + 2] & 0xff) << 8
-			| (data[i + 3] & 0xff);
-		  }
+  /**
+   * Gets the value of the tap messages magic field.
+   *
+   * @return The value of the magic field.
+   */
+  public final int getMagic() {
+    return mbytes[MAGIC_INDEX];
+  }
 
-	protected long decodeLong(byte[] data, int i) {
-		return (data[i] & 0xffL) << 56
-			| (data[i + 1] & 0xffL) << 48
-			| (data[i + 2] & 0xffL) << 40
-			| (data[i + 3] & 0xffL) << 32
-			| (data[i + 4] & 0xffL) << 24
-			| (data[i + 5] & 0xffL) << 16
-			| (data[i + 6] & 0xffL) << 8
-			| (data[i + 7] & 0xffL);
-	}
+  /**
+   * Sets the value of the tap messages opcode field.
+   *
+   * @param o The new value of the opcode field.
+   */
+  public final void setOpcode(TapOpcode o) {
+    mbytes[OPCODE_INDEX] = o.getOpcode();
+  }
+
+  /**
+   * Gets the value of the tap messages opaque field.
+   *
+   * @return The value of the opaque field.
+   */
+  public final TapOpcode getOpcode() {
+    return TapOpcode.getOpcodeByByte(mbytes[OPCODE_INDEX]);
+  }
+
+  /**
+   * Sets the key length for this message. This function should never be called
+   * by the user since changes to fields that affect key length call this
+   * function automatically.
+   *
+   * @param l The new value for the key length field.
+   */
+  protected final void setKeylength(long l) {
+    Util.valueToFieldOffest(mbytes, KEY_LENGTH_INDEX, KEY_LENGTH_FIELD_LENGTH,
+        l);
+  }
+
+  /**
+   * Gets the value of the tap messages key length field.
+   *
+   * @return The value of the key length field.
+   */
+  public final int getKeylength() {
+    return (int) Util.fieldToValue(mbytes, KEY_LENGTH_INDEX,
+        KEY_LENGTH_FIELD_LENGTH);
+  }
+
+  /**
+   * Sets the value of the tap messages data type field.
+   *
+   * @param b The new value for the data type field.
+   */
+  public final void setDatatype(byte b) {
+    mbytes[DATA_TYPE_INDEX] = b;
+  }
+
+  /**
+   * Gets the value of the tap messages data type field.
+   *
+   * @return The value of the data type field.
+   */
+  public final byte getDatatype() {
+    return mbytes[DATA_TYPE_INDEX];
+  }
+
+  /**
+   * Sets the value of the tap messages extra length field.
+   *
+   * @param i The new value for the extra length field.
+   */
+  public final void setExtralength(int i) {
+    mbytes[EXTRA_LENGTH_INDEX] = (byte) i;
+  }
+
+  /**
+   * Gets the value of the tap messages extra length field.
+   *
+   * @return The value of the extra length field.
+   */
+  public final int getExtralength() {
+    return mbytes[EXTRA_LENGTH_INDEX];
+  }
+
+  /**
+   * Sets the value of the tap messages vbucket field.
+   *
+   * @param vb The new value for the vbucket field.
+   */
+  public final void setVbucket(int vb) {
+    Util.valueToFieldOffest(mbytes, VBUCKET_INDEX, VBUCKET_FIELD_LENGTH, vb);
+  }
+
+  /**
+   * Gets the value of the tap messages vbucket field.
+   *
+   * @return The value of the vbucket field.
+   */
+  public final int getVbucket() {
+    return (int) Util.fieldToValue(mbytes, VBUCKET_INDEX, VBUCKET_FIELD_LENGTH);
+  }
+
+  /**
+   * Sets the value of the tap messages total body field.
+   *
+   * @param l The new value for the total body field.
+   */
+  public final void setTotalbody(long l) {
+    Util.valueToFieldOffest(mbytes, TOTAL_BODY_INDEX, TOTAL_BODY_FIELD_LENGTH,
+        l);
+  }
+
+  /**
+   * Gets the value of the tap messages total body field.
+   *
+   * @return The value of the total body field.
+   */
+  public final int getTotalbody() {
+    return (int) Util.fieldToValue(mbytes, TOTAL_BODY_INDEX,
+        TOTAL_BODY_FIELD_LENGTH);
+  }
+
+  /**
+   * Sets the value of the tap messages opaque field.
+   *
+   * @param op The new value for the opaque field.
+   */
+  public final void setOpaque(int op) {
+    Util.valueToFieldOffest(mbytes, OPAQUE_INDEX, OPAQUE_FIELD_LENGTH, op);
+  }
+
+  /**
+   * Gets the value of the tap messages opaque field.
+   *
+   * @return The value of the opaque field.
+   */
+  public final int getOpaque() {
+    return (int) Util.fieldToValue(mbytes, OPAQUE_INDEX, OPAQUE_FIELD_LENGTH);
+  }
+
+  /**
+   * Sets the value of the tap messages cas field.
+   *
+   * @param cas The new value for the cas field.
+   */
+  public final void setCas(long cas) {
+    Util.valueToFieldOffest(mbytes, CAS_INDEX, CAS_FIELD_LENGTH, cas);
+  }
+
+  /**
+   * Gets the value of the tap messages cas field.
+   *
+   * @return The value of the cas field.
+   */
+  public final long getCas() {
+    return Util.fieldToValue(mbytes, CAS_INDEX, CAS_FIELD_LENGTH);
+  }
+
+  /**
+   * Gets the length of the entire message.
+   *
+   * @return The length of the message.
+   */
+  public final int getMessageLength() {
+    return HEADER_LENGTH + getTotalbody();
+  }
+
+  /**
+   * Creates a ByteBuffer representation of the message.
+   *
+   * @return The ByteBuffer representation of the message.
+   */
+  public final ByteBuffer getBytes() {
+    return ByteBuffer.wrap(mbytes);
+  }
 }
--- src/main/java/net/spy/memcached/tapmessage/ResponseMessage.java
+++ src/main/java/net/spy/memcached/tapmessage/ResponseMessage.java
@@ -1,143 +1,191 @@
-package net.spy.memcached.tapmessage;
+/**
+ * Copyright (C) 2009-2011 Couchbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
+ * IN THE SOFTWARE.
+ */
 
-import java.util.List;
+package net.spy.memcached.tapmessage;
 
 /**
  * A representation of a tap stream message sent from a tap stream server.
  */
-public class ResponseMessage extends BaseMessage{
-	// Offsets are given from the end of the header
-	private static final int ENGINE_PRIVATE_OFFSET = 24;
-	private static final int FLAGS_OFFSET = 26;
-	private static final int TTL_OFFSET = 28;
-	private static final int RESERVED1_OFFSET = 29;
-	private static final int RESERVED2_OFFSET = 30;
-	private static final int RESERVED3_OFFSET = 31;
-	private static final int ITEM_FLAGS_OFFSET = 32;
-	private static final int ITEM_EXPIRY_OFFSET = 36;
-	private static final int KEY_OFFSET = 40;
+public class ResponseMessage extends BaseMessage {
+  // Offsets are given from the end of the header
+  private static final int ENGINE_PRIVATE_OFFSET = 0;
+  private static final int ENGINE_PRIVATE_FIELD_LENGTH = 2;
+  private static final int FLAGS_OFFSET = 2;
+  private static final int FLAGS_FIELD_LENGTH = 2;
+  private static final int TTL_OFFSET = 3;
+  private static final int TTL_FIELD_LENGTH = 1;
+  private static final int RESERVED1_OFFSET = 4;
+  private static final int RESERVED1_FIELD_LENGTH = 1;
+  private static final int RESERVED2_OFFSET = 5;
+  private static final int RESERVED2_FIELD_LENGTH = 1;
+  private static final int RESERVED3_OFFSET = 6;
+  private static final int RESERVED3_FIELD_LENGTH = 1;
+  private static final int ITEM_FLAGS_OFFSET = 7;
+  private static final int ITEM_FLAGS_FIELD_LENGTH = 4;
+  private static final int ITEM_EXPIRY_OFFSET = 11;
+  private static final int ITEM_EXPIRY_FIELD_LENGTH = 5;
 
-	private short engineprivate;
-	private List<TapFlag> flags;
-	private byte ttl;
-	private byte reserved1;
-	private byte reserved2;
-	private byte reserved3;
-	private int itemflags;
-	private int itemexpiry;
-	private String key;
-	private byte[] value;
+  /**
+   * Creates a ResponseMessage from binary data.
+   *
+   * @param buffer The binary data sent from the tap stream server.
+   */
+  public ResponseMessage(byte[] buffer) {
+    mbytes = buffer;
+  }
 
-	/**
-	 * Creates a ResponseMessage from binary data.
-	 * @param buffer The binary data sent from the tap stream server.
-	 */
-	public ResponseMessage(byte[] b) {
-		magic = TapMagic.getMagicByByte(b[MAGIC_OFFSET]);
-		opcode = TapOpcode.getOpcodeByByte(b[OPCODE_OFFSET]);
-		keylength = decodeShort(b, KEYLENGTH_OFFSET);
-		extralength = b[EXTRALENGTH_OFFSET];
-		datatype = b[DATATYPE_OFFSET];
-		vbucket = decodeShort(b, VBUCKET_OFFSET);
-		totalbody = decodeInt(b, TOTALBODY_OFFSET);
-		opaque = decodeInt(b, OPAQUE_OFFSET);
-		cas = decodeLong(b, CAS_OFFSET);
-		engineprivate = decodeShort(b, ENGINE_PRIVATE_OFFSET);
-		flags = TapFlag.getFlags(decodeShort(b, FLAGS_OFFSET));
-		ttl = b[TTL_OFFSET];
-		reserved1 = b[RESERVED1_OFFSET];
-		reserved2 = b[RESERVED2_OFFSET];
-		reserved3 = b[RESERVED3_OFFSET];
-		if (!opcode.equals(TapOpcode.OPAQUE)) {
-			if (opcode.equals(TapOpcode.MUTATION)) {
-				itemflags = decodeInt(b, ITEM_FLAGS_OFFSET);
-				itemexpiry = decodeInt(b, ITEM_EXPIRY_OFFSET);
-			}
-			byte[] keybytes = new byte[keylength];
-			System.arraycopy(b, KEY_OFFSET, keybytes, 0, keylength);
-			key = new String(keybytes);
-			value = new byte[b.length - keylength - KEY_OFFSET];
-			System.arraycopy(b, (keylength + KEY_OFFSET), value, 0, value.length);
-		}
-	}
+  /**
+   * Gets the value of the engine private field if the field exists in the
+   * message.
+   *
+   * @return The engine private data.
+   */
+  public long getEnginePrivate() {
+    if (ENGINE_PRIVATE_OFFSET + ENGINE_PRIVATE_FIELD_LENGTH
+        > getExtralength()) {
+      return 0;
+    }
+    int offset = HEADER_LENGTH + ENGINE_PRIVATE_OFFSET;
+    return Util.fieldToValue(mbytes, offset, ENGINE_PRIVATE_FIELD_LENGTH);
+  }
 
-	/**
-	 * Gets the value of the engine private field if the field exists in the message.
-	 * @return The engine private data.
-	 */
-	public long getEnginePrivate() {
-		return engineprivate;
-	}
+  /**
+   * Gets the value of the flags field if the field exists in the message.
+   *
+   * @return The flags data.
+   */
+  public int getFlags() {
+    if (FLAGS_OFFSET + FLAGS_FIELD_LENGTH > getExtralength()) {
+      return 0;
+    }
+    int offset = HEADER_LENGTH + FLAGS_OFFSET;
+    return (int) Util.fieldToValue(mbytes, offset, FLAGS_FIELD_LENGTH);
+  }
 
-	/**
-	 * Gets the value of the flags field if the field exists in the message.
-	 * @return The flags data.
-	 */
-	public List<TapFlag> getFlags() {
-		return flags;
-	}
+  /**
+   * Gets the value of the time to live field if the field exists in the
+   * message.
+   *
+   * @return The time to live value;
+   */
+  public int getTTL() {
+    if (TTL_OFFSET + TTL_FIELD_LENGTH > getExtralength()) {
+      return 0;
+    }
+    int offset = HEADER_LENGTH + TTL_OFFSET;
+    return (int) Util.fieldToValue(mbytes, offset, TTL_FIELD_LENGTH);
+  }
 
-	/**
-	 * Gets the value of the time to live field if the field exists in the message.
-	 * @return The time to live value;
-	 */
-	public int getTTL() {
-		return ttl;
-	}
+  /**
+   * Gets the value of the reserved1 field if the field exists in the message.
+   *
+   * @return The reserved1 data.
+   */
+  public int getReserved1() {
+    if (RESERVED1_OFFSET + RESERVED1_FIELD_LENGTH > getExtralength()) {
+      return 0;
+    }
+    int offset = HEADER_LENGTH + RESERVED1_OFFSET;
+    return (int) Util.fieldToValue(mbytes, offset, RESERVED1_FIELD_LENGTH);
+  }
 
-	/**
-	 * Gets the value of the reserved1 field if the field exists in the message.
-	 * @return The reserved1 data.
-	 */
-	public int getReserved1() {
-		return reserved1;
-	}
+  /**
+   * Gets the value of the reserved2 field if the field exists in the message.
+   *
+   * @return The reserved2 data.
+   */
+  public int getReserved2() {
+    if (RESERVED2_OFFSET + RESERVED2_FIELD_LENGTH > getExtralength()) {
+      return 0;
+    }
+    int offset = HEADER_LENGTH + RESERVED2_OFFSET;
+    return (int) Util.fieldToValue(mbytes, offset, RESERVED2_FIELD_LENGTH);
+  }
 
-	/**
-	 * Gets the value of the reserved2 field if the field exists in the message.
-	 * @return The reserved2 data.
-	 */
-	public int getReserved2() {
-		return reserved2;
-	}
+  /**
+   * Gets the value of the reserved3 field if the field exists in the message.
+   *
+   * @return The reserved3 data.
+   */
+  public int getReserved3() {
+    if (RESERVED3_OFFSET + RESERVED3_FIELD_LENGTH > getExtralength()) {
+      return 0;
+    }
+    int offset = HEADER_LENGTH + RESERVED3_OFFSET;
+    return (int) Util.fieldToValue(mbytes, offset, RESERVED3_FIELD_LENGTH);
+  }
 
-	/**
-	 * Gets the value of the reserved3 field if the field exists in the message.
-	 * @return The reserved3 data.
-	 */
-	public int getReserved3() {
-		return reserved3;
-	}
+  /**
+   * Gets the value of the items flag field if the field exists in the message.
+   *
+   * @return The items flag data.
+   */
+  public int getItemFlags() {
+    if (ITEM_FLAGS_OFFSET + ITEM_FLAGS_FIELD_LENGTH > getExtralength()) {
+      return 0;
+    }
+    int offset = HEADER_LENGTH + ITEM_FLAGS_OFFSET;
+    return (int) Util.fieldToValue(mbytes, offset, ITEM_FLAGS_FIELD_LENGTH);
+  }
 
-	/**
-	 * Gets the value of the items flag field if the field exists in the message.
-	 * @return The items flag data.
-	 */
-	public int getItemFlags() {
-		return itemflags;
-	}
+  /**
+   * Gets the value of the item expiry field if the field exists in the message.
+   *
+   * @return The item expiry data.
+   */
+  public long getItemExpiry() {
+    if (ITEM_EXPIRY_OFFSET + ITEM_EXPIRY_FIELD_LENGTH > getExtralength()) {
+      return 0;
+    }
+    int offset = HEADER_LENGTH + ITEM_EXPIRY_OFFSET;
+    return Util.fieldToValue(mbytes, offset, ITEM_EXPIRY_FIELD_LENGTH);
+  }
 
-	/**
-	 * Gets the value of the item expiry field if the field exists in the message.
-	 * @return The item expiry data.
-	 */
-	public long getItemExpiry() {
-		return itemexpiry;
-	}
+  /**
+   * Gets the value of the key field if the field exists in the message.
+   *
+   * @return The key data.
+   */
+  public String getKey() {
+    if (getExtralength() >= getTotalbody()) {
+      return new String();
+    }
+    int offset = (int) (HEADER_LENGTH + getExtralength());
+    return new String(mbytes, offset, getKeylength());
+  }
 
-	/**
-	 * Gets the value of the key field if the field exists in the message.
-	 * @return The key data.
-	 */
-	public String getKey() {
-		return key;
-	}
-
-	/**
-	 * Gets the value of the value field if the field exists in the message.
-	 * @return The value data.
-	 */
-	public byte[] getValue() {
-		return value;
-	}
+  /**
+   * Gets the value of the value field if the field exists in the message.
+   *
+   * @return The value data.
+   */
+  public byte[] getValue() {
+    if (getExtralength() + getKeylength() >= getTotalbody()) {
+      return new byte[0];
+    }
+    int offset = (int) (HEADER_LENGTH + getExtralength() + getKeylength());
+    int length = (int) (getTotalbody() - getKeylength() - getExtralength());
+    byte[] value = new byte[length];
+    System.arraycopy(mbytes, offset, value, 0, length);
+    return value;
+  }
 }

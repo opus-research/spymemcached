7b422376c2e02191820338b2be69357068e719c2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:59 +0000

    First commited as before_7650_rev3

--- src/test/java/net/spy/memcached/TestConfig.java
+++
@@ -1,6 +0,0 @@
-package net.spy.memcached;
-
-public class TestConfig {
-	public static final String IPV4_ADDR = System.getProperty("server.address", "127.0.0.1");
-	public static final String IPV6_ADDR = "::ffff:" + System.getProperty("server.address", "127.0.0.1");
-}

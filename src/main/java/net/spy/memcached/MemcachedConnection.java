579c57efbb9d73fb5380a51e6003249c173c7db3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:46 +0000

    First commited as before_32337_rev4

--- src/main/java/net/spy/memcached/MemcachedConnection.java
+++ src/main/java/net/spy/memcached/MemcachedConnection.java
@@ -1363,12 +1363,4 @@ public class MemcachedConnection extends SpyThread {
     }
   }
 
-  /**
-   * Returns whether the connection is shut down or not.
-   *
-   * @return true if the connection is shut down, false otherwise.
-   */
-  public boolean isShutDown() {
-    return shutDown;
-  }
 }

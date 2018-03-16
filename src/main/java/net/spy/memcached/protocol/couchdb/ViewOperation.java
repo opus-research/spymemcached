fbf6942016556be993b016af63c9c5df3d7eea61
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:17 +0000

    First commited as before_8401_rev5

--- src/main/java/net/spy/memcached/protocol/couchdb/ViewOperation.java
+++
@@ -1,9 +0,0 @@
-package net.spy.memcached.protocol.couchdb;
-
-import net.spy.memcached.ops.OperationCallback;
-
-public interface ViewOperation {
-	public interface ViewCallback extends OperationCallback {
-		void gotData(View view);
-	}
-}
--- src/test/java/net/spy/memcached/couch/TestOperation.java
+++
@@ -1,9 +0,0 @@
-package net.spy.memcached.couch;
-
-import net.spy.memcached.ops.OperationCallback;
-
-public interface TestOperation {
-	interface TestCallback extends OperationCallback {
-		void getData(String response);
-	}
-}

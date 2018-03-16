fa109adfae4c45fe01c1d81eb40da0374c4878e4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:29 +0000

    First commited as after_6753_rev1

---
+++ src/main/java/net/spy/memcached/protocol/ascii/TouchOperationImpl.java
@@ -0,0 +1,54 @@
+package net.spy.memcached.protocol.ascii;
+
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.Collections;
+
+import net.spy.memcached.KeyUtil;
+import net.spy.memcached.ops.KeyedOperation;
+import net.spy.memcached.ops.OperationCallback;
+import net.spy.memcached.ops.OperationState;
+import net.spy.memcached.ops.OperationStatus;
+
+public class TouchOperationImpl extends OperationImpl 
+	implements KeyedOperation {
+	private static final int OVERHEAD = 32;
+	private static final String CMD = "touch";
+	
+	private final String key;
+	private final int exp;
+	
+	private static final OperationStatus STORED=
+		new OperationStatus(true, "STORED");
+	private static final OperationStatus NOT_FOUND=
+		new OperationStatus(false, "NOT_FOUND");
+	private static final OperationStatus TEMP_FAIL =
+    	new OperationStatus(false, "Temporary Error");
+	
+	public TouchOperationImpl(String k, int e, OperationCallback cb) {
+		super(cb);
+		key=k;
+		exp=e;
+	}
+
+	@Override
+	public void handleLine(String line) {
+		getLogger().debug("Delete of %s returned %s", key, line);
+		getCallback().receivedStatus(matchStatus(line, STORED, NOT_FOUND, TEMP_FAIL));
+		transitionState(OperationState.COMPLETE);
+	}
+
+	@Override
+	public void initialize() {
+		ByteBuffer b=ByteBuffer.allocate(
+			KeyUtil.getKeyBytes(key).length + OVERHEAD);
+		setArguments(b, CMD, key, exp);
+		b.flip();
+		setBuffer(b);
+	}
+
+	@Override
+	public Collection<String> getKeys() {
+		return Collections.singleton(key);
+	}
+}
---
+++ src/main/java/net/spy/memcached/protocol/binary/TouchOperationImpl.java
@@ -0,0 +1,55 @@
+package net.spy.memcached.protocol.binary;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import net.spy.memcached.ops.KeyedOperation;
+import net.spy.memcached.ops.OperationCallback;
+import net.spy.memcached.ops.OperationStatus;
+
+public class TouchOperationImpl extends OperationImpl
+	implements KeyedOperation {
+	
+	static final int CMD=28;
+	
+	private final String key;
+	private final int exp;
+
+	protected TouchOperationImpl(String k, int e, OperationCallback cb) {
+		super(CMD, generateOpaque(), cb);
+		key=k;
+		exp=e;
+	}
+
+	@Override
+	public Collection<String> getKeys() {
+		return Collections.singleton(key);
+	}
+	
+	@Override
+	protected OperationStatus getStatusForErrorCode(int errCode, byte[] errPl) {
+        OperationStatus baseStatus = super.getStatusForErrorCode(errCode, errPl);
+        if (baseStatus != null) {
+            return baseStatus;
+        }
+		OperationStatus rv=null;
+		switch(errCode) {
+			case ERR_EXISTS:
+				rv=EXISTS_STATUS;
+				break;
+			case ERR_NOT_FOUND:
+				rv=NOT_FOUND_STATUS;
+				break;
+			case ERR_TEMP_FAIL:
+				rv=TEMP_FAIL;
+				break;
+		}
+		return rv;
+	}
+
+	@Override
+	public void initialize() {
+		prepareBuffer(key, 0, EMPTY_BYTES, exp);
+	}
+
+}

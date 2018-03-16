93c6ef8399d90604d0c1d0b28efab2d396130d85
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:48:48 +0000

    First commited as before_11099_rev8

--- src/main/java/net/spy/memcached/protocol/couch/ViewsFetcherOperation.java
+++ src/main/java/net/spy/memcached/protocol/couch/ViewsFetcherOperation.java
@@ -1,5 +1,4 @@
 /**
- * Copyright (C) 2006-2009 Dustin Sallings
  * Copyright (C) 2009-2011 Couchbase, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,51 +20,21 @@
  * IN THE SOFTWARE.
  */
 
-package net.spy.memcached;
+package net.spy.memcached.protocol.couch;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
-import net.spy.memcached.vbucket.config.Config;
+import net.spy.memcached.ops.OperationCallback;
 
 /**
- * Interface for locating a node by hash value.
+ * A ViewsOperation.
  */
-public interface NodeLocator {
+public interface ViewsFetcherOperation {
 
   /**
-   * Get the primary location for the given key.
-   *
-   * @param k the object key
-   * @return the QueueAttachment containing the primary storage for a key
+   * A ViewsCallback.
    */
-  MemcachedNode getPrimary(String k);
-
-  /**
-   * Get an iterator over the sequence of nodes that make up the backup
-   * locations for a given key.
-   *
-   * @param k the object key
-   * @return the sequence of backup nodes.
-   */
-  Iterator<MemcachedNode> getSequence(String k);
-
-  /**
-   * Get all memcached nodes. This is useful for broadcasting messages.
-   */
-  Collection<MemcachedNode> getAll();
-
-  /**
-   * Create a read-only copy of this NodeLocator.
-   */
-  NodeLocator getReadonlyCopy();
-
-  /**
-   * Update locator status.
-   *
-   * @param nodes New locator nodes.
-   * @param conf Locator configuration.
-   */
-  void updateLocator(final List<MemcachedNode> nodes, final Config conf);
+  interface ViewsFetcherCallback extends OperationCallback {
+    void gotData(List<View> views);
+  }
 }
--- src/main/java/net/spy/memcached/vbucket/config/ConfigurationParser.java
+++ src/main/java/net/spy/memcached/vbucket/config/ConfigurationParser.java
@@ -1,5 +1,4 @@
 /**
- * Copyright (C) 2006-2009 Dustin Sallings
  * Copyright (C) 2009-2011 Couchbase, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,51 +20,20 @@
  * IN THE SOFTWARE.
  */
 
-package net.spy.memcached;
+package net.spy.memcached.vbucket.config;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-
-import net.spy.memcached.vbucket.config.Config;
+import java.text.ParseException;
+import java.util.Map;
 
 /**
- * Interface for locating a node by hash value.
+ * A ConfigParser.
  */
-public interface NodeLocator {
+public interface ConfigurationParser {
+  Map<String, Pool> parseBase(final String base) throws ParseException;
 
-  /**
-   * Get the primary location for the given key.
-   *
-   * @param k the object key
-   * @return the QueueAttachment containing the primary storage for a key
-   */
-  MemcachedNode getPrimary(String k);
+  Map<String, Bucket> parseBuckets(String buckets) throws ParseException;
 
-  /**
-   * Get an iterator over the sequence of nodes that make up the backup
-   * locations for a given key.
-   *
-   * @param k the object key
-   * @return the sequence of backup nodes.
-   */
-  Iterator<MemcachedNode> getSequence(String k);
+  Bucket parseBucket(String sBucket) throws ParseException;
 
-  /**
-   * Get all memcached nodes. This is useful for broadcasting messages.
-   */
-  Collection<MemcachedNode> getAll();
-
-  /**
-   * Create a read-only copy of this NodeLocator.
-   */
-  NodeLocator getReadonlyCopy();
-
-  /**
-   * Update locator status.
-   *
-   * @param nodes New locator nodes.
-   * @param conf Locator configuration.
-   */
-  void updateLocator(final List<MemcachedNode> nodes, final Config conf);
+  void loadPool(Pool pool, String sPool) throws ParseException;
 }

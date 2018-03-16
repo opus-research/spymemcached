32fe8b931b1a183d8974ac7c363082ff32af79d9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:13 +0000

    First commited as after_569_rev1

--- src/main/java/net/spy/memcached/internal/BulkFuture.java
+++ src/main/java/net/spy/memcached/internal/BulkFuture.java
@@ -1,12 +1,12 @@
 package net.spy.memcached.internal;
 
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Provides some additional flexibility.
+ * Provides some additional flexibility for async getBulk operations: getSome
+ * allows getting partial data when timeout is reached.
  * 
  * @author boris.partensky@gmail.com
  * @param <V>
@@ -16,17 +16,17 @@ public interface BulkFuture<V> extends Future<V> {
 	
 	public boolean isTimeout();
 
-	/**
-	 * this method does not throw timeout exception. Instead, if timeout is reached,
-	 * it returns what's there and sets isTimeout flag. 
-     *
-	 * 
-	 * @param timeout
-	 * @param unit
-	 * @return
-	 * @throws InterruptedException
-	 * @throws ExecutionException
-	 */
+    /**
+     * this method does not throw timeout exception. Instead, if timeout is
+     * reached, it returns what's there and sets isTimeout flag. This allows to
+     * get partial data when timeout is reached.
+     * 
+     * @param timeout
+     * @param unit
+     * @return
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
 	public V getSome(long to, TimeUnit unit)
 			throws InterruptedException, ExecutionException;
 

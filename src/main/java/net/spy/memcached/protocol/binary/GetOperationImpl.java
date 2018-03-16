072626073ea58abc6d38328c901db263ff1ce831
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:06 +0000

    First commited as before_7877_rev2

--- src/main/java/net/spy/memcached/protocol/binary/GetOperationImpl.java
+++ src/main/java/net/spy/memcached/protocol/binary/GetOperationImpl.java
@@ -1,9 +1,13 @@
 package net.spy.memcached.protocol.binary;
 
+import net.spy.memcached.ops.GetAndTouchOperation;
 import net.spy.memcached.ops.GetOperation;
+import net.spy.memcached.ops.GetlOperation;
+import net.spy.memcached.ops.GetsOperation;
+import net.spy.memcached.ops.OperationCallback;
 
 class GetOperationImpl extends SingleKeyOperationImpl
-	implements GetOperation {
+	implements GetOperation, GetsOperation, GetlOperation, GetAndTouchOperation {
 
 	static final int GET_CMD=0x00;
 	static final int GETL_CMD=0x94;
@@ -14,13 +18,35 @@ class GetOperationImpl extends SingleKeyOperationImpl
 	 */
 	static final int EXTRA_HDR_LEN=4;
 
+	private final int exp;
+
 	public GetOperationImpl(String k, GetOperation.Callback cb) {
 		super(GET_CMD, generateOpaque(), k, cb);
+		exp=0;
+	}
+
+	public GetOperationImpl(String k, GetsOperation.Callback cb) {
+		super(GET_CMD, generateOpaque(), k, cb);
+		exp=0;
+	}
+
+	public GetOperationImpl(String k, int e, GetlOperation.Callback cb) {
+		super(GETL_CMD, generateOpaque(), k, cb);
+		exp=e;
+	}
+
+	public GetOperationImpl(String k, int e, GetAndTouchOperation.Callback cb) {
+		super(GAT_CMD, generateOpaque(), k, cb);
+		exp=e;
 	}
 
 	@Override
 	public void initialize() {
-		prepareBuffer(key, 0, EMPTY_BYTES);
+		if (exp > 0) {
+			prepareBuffer(key, 0, EMPTY_BYTES, exp);
+		} else {
+			prepareBuffer(key, 0, EMPTY_BYTES);
+		}
 	}
 
 	@Override
@@ -28,8 +54,23 @@ class GetOperationImpl extends SingleKeyOperationImpl
 		final int flags=decodeInt(pl, 0);
 		final byte[] data=new byte[pl.length - EXTRA_HDR_LEN];
 		System.arraycopy(pl, EXTRA_HDR_LEN, data, 0, pl.length-EXTRA_HDR_LEN);
-		GetOperation.Callback gcb=(GetOperation.Callback)getCallback();
-		gcb.gotData(key, flags, data);
+		// Assume we're processing a get unless the cast fails.
+		OperationCallback cb = getCallback();
+		if (cb instanceof GetOperation.Callback) {
+			GetOperation.Callback gcb=(GetOperation.Callback)cb;
+			gcb.gotData(key, flags, data);
+		} else if (cb instanceof GetsOperation.Callback) {
+			GetsOperation.Callback gcb=(GetsOperation.Callback)cb;
+			gcb.gotData(key, flags, responseCas, data);
+		} else if (cb instanceof GetlOperation.Callback) {
+			GetlOperation.Callback gcb=(GetlOperation.Callback)cb;
+			gcb.gotData(key, flags, responseCas, data);
+		} else if (cb instanceof GetAndTouchOperation.Callback) {
+			GetAndTouchOperation.Callback gcb=(GetAndTouchOperation.Callback)cb;
+			gcb.gotData(key, flags, responseCas, data);
+		} else {
+			throw new ClassCastException("Couldn't convert " + cb + "to a relevent op");
+		}
 		getCallback().receivedStatus(STATUS_OK);
 	}
 

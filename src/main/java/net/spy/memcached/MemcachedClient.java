5884e1c60855971b661e5eb059e0b22fe82a5e44
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:49 +0000

    First commited as before_33700_rev5

--- src/main/java/net/spy/memcached/MemcachedClient.java
+++ src/main/java/net/spy/memcached/MemcachedClient.java
@@ -45,7 +45,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -1315,8 +1314,7 @@ public class MemcachedClient extends SpyObject implements MemcachedClientIF,
       ks.add(key);
     }
 
-    final AtomicInteger pendingChunks = new AtomicInteger(chunks.size());
-    final CountDownLatch latch = new CountDownLatch(1);
+    final CountDownLatch latch = new CountDownLatch(chunks.size());
     final Collection<Operation> ops = new ArrayList<Operation>(chunks.size());
     final BulkGetFuture<T> rv = new BulkGetFuture<T>(m, ops, latch, executorService);
 
@@ -1324,9 +1322,6 @@ public class MemcachedClient extends SpyObject implements MemcachedClientIF,
       @Override
       @SuppressWarnings("synthetic-access")
       public void receivedStatus(OperationStatus status) {
-        if (status.getStatusCode() == StatusCode.ERR_NOT_MY_VBUCKET) {
-          pendingChunks.addAndGet(Integer.parseInt(status.getMessage()));
-        }
         rv.setStatus(status);
       }
 
@@ -1339,8 +1334,8 @@ public class MemcachedClient extends SpyObject implements MemcachedClientIF,
 
       @Override
       public void complete() {
-        if (pendingChunks.decrementAndGet() <= 0) {
-          latch.countDown();
+        latch.countDown();
+        if (latch.getCount() == 0) {
           rv.signalComplete();
         }
       }

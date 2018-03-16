4fabb2b66b14fff68b8787ab6f8d10d12fec28ac
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:30 +0000

    First commited as after_6753_rev6

--- src/main/java/net/spy/memcached/protocol/ascii/AsciiOperationFactory.java
+++ src/main/java/net/spy/memcached/protocol/ascii/AsciiOperationFactory.java
@@ -68,6 +68,10 @@ public class AsciiOperationFactory extends BaseOperationFactory {
 		return new StoreOperationImpl(storeType, key, flags, exp, data, cb);
 	}
 
+	public KeyedOperation touch(String key, int expiration, OperationCallback cb) {
+		throw new UnsupportedOperationException();
+	}
+
 	public VersionOperation version(OperationCallback cb) {
 		return new VersionOperationImpl(cb);
 	}

5aa0b570ae7d0d7b9736c168758e475ce36ad443
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:25 +0000

    First commited as before_25741_rev2

--- src/main/java/net/spy/memcached/compat/CloseUtil.java
+++ src/main/java/net/spy/memcached/compat/CloseUtil.java
@@ -24,8 +24,8 @@ package net.spy.memcached.compat;
 
 import java.io.Closeable;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.spy.memcached.compat.log.Logger;
+import net.spy.memcached.compat.log.LoggerFactory;
 
 /**
  * CloseUtil exists to provide a safe means to close anything closeable. This

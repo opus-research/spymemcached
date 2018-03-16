d436b8f2300e25fad960b40d6da2f1a81f144e38
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:48:05 +0000

    First commited as before_9227_rev1

--- src/main/java/net/spy/memcached/protocol/ascii/AsciiMemcachedNodeImpl.java
+++ src/main/java/net/spy/memcached/protocol/ascii/AsciiMemcachedNodeImpl.java
@@ -64,7 +64,7 @@ public final class AsciiMemcachedNodeImpl extends TCPMemcachedNodeImpl {
 
         // Initialize the new mega get
         optimizedOp.initialize();
-        assert optimizedOp.getState() == OperationState.WRITE_QUEUED;
+        assert optimizedOp.getState() == OperationState.WRITING;
         ProxyCallback pcb = (ProxyCallback) og.getCallback();
         getLogger().debug("Set up %s with %s keys and %s callbacks", this,
             pcb.numKeys(), pcb.numCallbacks());

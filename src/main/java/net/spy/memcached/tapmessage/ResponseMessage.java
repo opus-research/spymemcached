e78252dbf5db0e5ca78a45e0e7420bf366e900a5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:05 +0000

    First commited as before_13571_rev2

--- src/main/java/net/spy/memcached/tapmessage/ResponseMessage.java
+++ src/main/java/net/spy/memcached/tapmessage/ResponseMessage.java
@@ -104,20 +104,6 @@ public class ResponseMessage extends BaseMessage {
       key = new byte[0];
       value = new byte[0];
       revid = new byte[0];
-    } else if (opcode.equals(TapOpcode.START_CHECKPOINT) || opcode.equals(TapOpcode.END_CHECKPOINT)) {
-        itemflags = 0;
-        itemexpiry = 0;
-        vbucketstate = (int)decodeLong(b, KEY_OFFSET);
-        key = new byte[0];
-        value = new byte[0];
-        revid = new byte[0];
-    } else if(opcode.equals(TapOpcode.OPAQUE)) {
-        itemflags = 0;
-        itemexpiry = 0;
-        vbucketstate = (int)decodeInt(b, ITEM_FLAGS_OFFSET);
-        key = new byte[0];
-        value = new byte[0];
-        revid = new byte[0];
     } else {
       itemflags = 0;
       itemexpiry = 0;

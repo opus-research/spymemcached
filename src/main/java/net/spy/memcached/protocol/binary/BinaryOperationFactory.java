15b6d9154d087d6c7eae3b0feee460e4bf989d00
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:02 +0000

    First commited as before_12992_rev1

--- src/main/java/net/spy/memcached/protocol/binary/BinaryOperationFactory.java
+++ src/main/java/net/spy/memcached/protocol/binary/BinaryOperationFactory.java
@@ -55,7 +55,6 @@ import net.spy.memcached.ops.StatsOperation;
 import net.spy.memcached.ops.StoreOperation;
 import net.spy.memcached.ops.StoreType;
 import net.spy.memcached.ops.TapOperation;
-import net.spy.memcached.ops.UnlockOperation;
 import net.spy.memcached.ops.VersionOperation;
 import net.spy.memcached.tapmessage.RequestMessage;
 import net.spy.memcached.tapmessage.TapOpcode;
@@ -70,10 +69,6 @@ public class BinaryOperationFactory extends BaseOperationFactory {
     return new DeleteOperationImpl(key, operationCallback);
   }
 
-  public UnlockOperation unlock(String key, long casId,
-          OperationCallback cb) {
-    return new UnlockOperationImpl(key, casId, cb);
-  }
   public FlushOperation flush(int delay, OperationCallback cb) {
     return new FlushOperationImpl(cb);
   }

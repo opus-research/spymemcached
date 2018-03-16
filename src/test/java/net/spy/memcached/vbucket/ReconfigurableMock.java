8662ed45322e6aa039482becbb5ad5bb54e1a2c4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:31 +0000

    First commited as before_6760_rev4

--- src/test/java/net/spy/memcached/vbucket/ReconfigurableMock.java
+++
@@ -1,8 +0,0 @@
-package net.spy.memcached.vbucket;
-
-import net.spy.memcached.vbucket.config.Bucket;
-
-public class ReconfigurableMock implements Reconfigurable {
-    public void reconfigure(Bucket bucket) {
-    }
-}

e11e2358f7ba3a1a2ca6dbcc195302ec36f46137
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:35 +0000

    First commited as before_6817_rev6

--- src/main/java/net/spy/memcached/ops/VBucketAware.java
+++ src/main/java/net/spy/memcached/ops/VBucketAware.java
@@ -1,12 +1,5 @@
 package net.spy.memcached.ops;
 
-import net.spy.memcached.MemcachedNode;
-
-import java.util.Collection;
-
 public interface VBucketAware {
     void setVBucket(int vbucket);
-    Collection<MemcachedNode> getNotMyVbucketNodes();
-    void addNotMyVbucketNode(MemcachedNode node);
-    void setNotMyVbucketNodes(Collection<MemcachedNode> nodes);
 }

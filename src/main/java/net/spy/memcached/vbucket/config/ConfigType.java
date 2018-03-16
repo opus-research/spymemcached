89da84fe248b78f943e57fc3ee523b000ee9bda6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:44 +0000

    First commited as before_6849_rev2

--- src/main/java/net/spy/memcached/vbucket/config/ConfigType.java
+++ src/main/java/net/spy/memcached/vbucket/config/ConfigType.java
@@ -1,16 +1,14 @@
 package net.spy.memcached.vbucket.config;
 
 /**
- * Config may have types.  These types are associated with the individual bucket
- * types as discerned from the configuration for the bucket served up by the
- * Membase server.
  *
+ * @author ingenthr
  */
 public enum ConfigType {
 	/**
-	 * Memcache bucket type.
+	 * Cache bucket type.
 	 */
-	MEMCACHE,
+	CACHE,
 	/**
 	 * Membase bucket type.
 	 */

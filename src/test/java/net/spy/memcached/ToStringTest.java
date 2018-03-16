8a34e8b5bd072ceba1ea206292b5f3b0a80b2283
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:48:23 +0000

    First commited as after_9472_rev1

---
+++ src/test/java/net/spy/memcached/ToStringTest.java
@@ -0,0 +1,23 @@
+package net.spy.memcached;
+
+/**
+ * These tests test to make sure that we don't get null pointer
+ * exceptions when calling toString methods on classes that have
+ * custom toString() functions.
+ */
+public class ToStringTest {
+
+	public void testDefaultConnectionFactory() {
+		(new DefaultConnectionFactory()).toString();
+		(new DefaultConnectionFactory(10, 1000)).toString();
+		(new DefaultConnectionFactory(100, 100,
+				HashAlgorithm.KETAMA_HASH)).toString();
+	}
+
+	public void testBinaryConnectionFactory() {
+		(new BinaryConnectionFactory()).toString();
+		(new BinaryConnectionFactory(10, 1000)).toString();
+		(new BinaryConnectionFactory(100, 1000,
+				HashAlgorithm.KETAMA_HASH)).toString();
+	}
+}

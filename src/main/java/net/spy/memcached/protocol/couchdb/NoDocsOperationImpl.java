22e2dfb9e76ebab24122c131dcf6d3bd89ee25e3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:18 +0000

    First commited as after_8461_rev3

--- src/main/java/net/spy/memcached/protocol/couchdb/NoDocsOperationImpl.java
+++ src/main/java/net/spy/memcached/protocol/couchdb/NoDocsOperationImpl.java
@@ -41,22 +41,33 @@ public class NoDocsOperationImpl extends HttpOperationImpl implements
 	private ViewResponseNoDocs parseNoDocsViewResult(String json)
 			throws ParseException {
 		final Collection<RowNoDocs> rows = new LinkedList<RowNoDocs>();
+		final Collection<RowError> errors = new LinkedList<RowError>();
 		if (json != null) {
 			try {
 				JSONObject base = new JSONObject(json);
 				if (base.has("rows")) {
 					JSONArray ids = base.getJSONArray("rows");
 					for (int i = 0; i < ids.length(); i++) {
-						String id = ids.getJSONObject(i).getString("id");
-						String key = ids.getJSONObject(i).getString("key");
-						String value = ids.getJSONObject(i).getString("value");
-						rows.add(new RowNoDocs(id, key, value));
+						JSONObject elem = ids.getJSONObject(i);
+						if (elem.has("id")) {
+							String id = elem.getString("id");
+							String key = elem.getString("key");
+							String value = elem.getString("value");
+							rows.add(new RowNoDocs(id, key, value));
+						} else if (elem.has("error")) {
+							String from = elem.getString("from");
+							String reason = elem.getString("reason");
+							errors.add(new RowError(from, reason));
+						} else {
+							throw new ParseException("Unexpected row at line "
+									+ i + ": " + json, 0);
+						}
 					}
 				}
 			} catch (JSONException e) {
 				throw new ParseException("Cannot read json: " + json, 0);
 			}
 		}
-		return new ViewResponseNoDocs(rows);
+		return new ViewResponseNoDocs(rows, errors);
 	}
 }

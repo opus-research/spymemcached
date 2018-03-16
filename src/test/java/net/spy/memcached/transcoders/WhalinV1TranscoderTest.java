d83da445884ca86caca264b76eec1368e0e12722
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:48:44 +0000

    First commited as before_10299_rev1

--- src/test/java/net/spy/memcached/transcoders/WhalinV1TranscoderTest.java
+++ src/test/java/net/spy/memcached/transcoders/WhalinV1TranscoderTest.java
@@ -49,13 +49,16 @@ public class WhalinV1TranscoderTest extends BaseTranscoderCase {
 
   public void testJsonObject() {
     WhalinV1Transcoder transcoder = ((WhalinV1Transcoder)getTranscoder());
-    String json = "{\"aaaaaaaaaaaaaaaaaaaaaaaaa\":\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"}";
+    String json = "{\"aaaaaaaaaaaaaaaaaaaaaaaaa\":"
+        + "\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"}";
     transcoder.setCompressionThreshold(8);
     CachedData cd = transcoder.encode(json);
-    assertNotSame(SerializingTranscoder.COMPRESSED, cd.getFlags());
-    assertTrue(Arrays.equals(json.getBytes(),
+    assertFalse("Flags shows JSON was compressed",
+        (cd.getFlags() & (1L << WhalinV1Transcoder.COMPRESSED)) != 0);
+    assertTrue("JSON was incorrectly encoded", Arrays.equals(json.getBytes(),
         Arrays.copyOfRange(cd.getData(), 1, cd.getData().length)));
-    assertEquals(json, transcoder.decode(cd));
+    assertEquals("JSON was harmed, should not have been",
+        json, transcoder.decode(cd));
   }
 
   @Override

1bce8ecde4422ea8d8e16e160d9af0f50a776e77
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:40 +0000

    First commited as after_6823_rev2

--- src/main/java/net/spy/memcached/MemcachedClient.java
+++ src/main/java/net/spy/memcached/MemcachedClient.java
@@ -57,6 +57,7 @@ import net.spy.memcached.vbucket.ConfigurationProvider;
 import net.spy.memcached.vbucket.ConfigurationProviderHTTP;
 import net.spy.memcached.vbucket.Reconfigurable;
 import net.spy.memcached.vbucket.config.Bucket;
+import net.spy.memcached.vbucket.config.ConfigType;
 
 /**
  * Client to a memcached server.
@@ -189,6 +190,18 @@ public class MemcachedClient extends SpyThread
 		start();
 	}
 
+    /**
+     * Get a MemcachedClient based on the REST response from a Membase server.
+     *
+     * @param baseList
+     * @param bucketName
+     * @param usr
+     * @param pwd
+     * @param isVBucketAware
+     * @throws IOException
+     * @throws ConfigurationException
+     * @deprecated Use the constructor without the isVbucketAware
+     */
     public MemcachedClient(final List<URI> baseList,
                            final String bucketName,
                            final String usr, final String pwd,
@@ -215,6 +228,82 @@ public class MemcachedClient extends SpyThread
                     .setLocatorType(ConnectionFactoryBuilder.Locator.CONSISTENT);
 
         }
+        if (!this.configurationProvider.getAnonymousAuthBucket().equals(bucketName) && usr != null) {
+            AuthDescriptor ad = new AuthDescriptor(new String[]{"PLAIN"},
+                    new PlainCallbackHandler(usr, pwd));
+            cfb.setAuthDescriptor(ad);
+        }
+        ConnectionFactory cf = cfb.build();
+        List<InetSocketAddress> addrs = AddrUtil.getAddresses(bucket.getConfig().getServers());
+        if(cf == null) {
+            throw new NullPointerException("Connection factory required");
+        }
+        if(addrs == null) {
+            throw new NullPointerException("Server list required");
+        }
+        if(addrs.isEmpty()) {
+            throw new IllegalArgumentException(
+                "You must have at least one server to connect to");
+        }
+        if(cf.getOperationTimeout() <= 0) {
+            throw new IllegalArgumentException(
+                "Operation timeout must be positive.");
+        }
+        tcService = new TranscodeService(cf.isDaemon());
+        transcoder=cf.getDefaultTranscoder();
+        opFact=cf.getOperationFactory();
+        assert opFact != null : "Connection factory failed to make op factory";
+        conn=cf.createConnection(addrs);
+        assert conn != null : "Connection factory failed to make a connection";
+        operationTimeout = cf.getOperationTimeout();
+        authDescriptor = cf.getAuthDescriptor();
+        if(authDescriptor != null) {
+            addObserver(this);
+        }
+        setName("Memcached IO over " + conn);
+        setDaemon(cf.isDaemon());
+        this.configurationProvider.subscribe(bucketName, this);
+        start();
+    }
+
+    /**
+     * Get a MemcachedClient based on the REST response from a Membase server.
+     *
+     * @param baseList
+     * @param bucketName
+     * @param usr
+     * @param pwd
+     * @throws IOException
+     * @throws ConfigurationException
+     */
+    public MemcachedClient(final List<URI> baseList,
+                           final String bucketName,
+                           final String usr, final String pwd) throws IOException, ConfigurationException {
+        for (URI bu : baseList) {
+            if (!bu.isAbsolute()) {
+                throw new IllegalArgumentException("The base URI must be absolute");
+            }
+        }
+
+        this.configurationProvider = new ConfigurationProviderHTTP(baseList, usr, pwd);
+        Bucket bucket = this.configurationProvider.getBucketConfiguration(bucketName);
+	net.spy.memcached.vbucket.config.Config config = bucket.getConfig();
+        ConnectionFactoryBuilder cfb = new ConnectionFactoryBuilder();
+        if (config.getConfigType() == ConfigType.MEMBASE) {
+            cfb.setFailureMode(FailureMode.Retry)
+                    .setProtocol(ConnectionFactoryBuilder.Protocol.BINARY)
+                    .setHashAlg(HashAlgorithm.KETAMA_HASH)
+                    .setLocatorType(ConnectionFactoryBuilder.Locator.VBUCKET)
+                    .setVBucketConfig(bucket.getConfig());
+        } else if (config.getConfigType() == ConfigType.CACHE) {
+            cfb.setFailureMode(FailureMode.Retry)
+                    .setProtocol(ConnectionFactoryBuilder.Protocol.BINARY)
+                    .setHashAlg(HashAlgorithm.KETAMA_HASH)
+                    .setLocatorType(ConnectionFactoryBuilder.Locator.CONSISTENT);
+
+        } else {
+	    throw new ConfigurationException("Bucket type not supported or JSON response unexpected");
+	}
         if (!this.configurationProvider.getAnonymousAuthBucket().equals(bucketName) && usr != null) {
             AuthDescriptor ad = new AuthDescriptor(new String[]{"PLAIN"},
                     new PlainCallbackHandler(usr, pwd));

66e6cf66281cf63c371fea685a32d82de42a0474
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:17 +0000

    First commited as before_3617_rev1

--- src/main/java/net/spy/memcached/protocol/BaseOperationImpl.java
+++ src/main/java/net/spy/memcached/protocol/BaseOperationImpl.java
@@ -2,8 +2,6 @@ package net.spy.memcached.protocol;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Collection;
-import java.util.HashSet;
 
 import net.spy.memcached.MemcachedNode;
 import net.spy.memcached.compat.SpyObject;
@@ -30,8 +28,6 @@ public abstract class BaseOperationImpl extends SpyObject {
 	private OperationException exception = null;
 	protected OperationCallback callback = null;
 	private volatile MemcachedNode handlingNode = null;
-    protected int vbucket;
-    protected Collection<MemcachedNode> notMyVbucketNodes = new HashSet<MemcachedNode>();
 
 	public BaseOperationImpl() {
 		super();
@@ -146,16 +142,5 @@ public abstract class BaseOperationImpl extends SpyObject {
 	public void setHandlingNode(MemcachedNode to) {
 		handlingNode = to;
 	}
-    public void setVBucket(int vbucket) {
-        this.vbucket = vbucket;
-    }
-    public Collection<MemcachedNode> getNotMyVbucketNodes() {
-        return notMyVbucketNodes;
-    }
-    public void addNotMyVbucketNode(MemcachedNode node) {
-        notMyVbucketNodes.add(node);
-    }
-    public void setNotMyVbucketNodes(Collection<MemcachedNode> nodes) {
-        notMyVbucketNodes = nodes;
-    }
+
 }
--- src/main/java/net/spy/memcached/vbucket/config/Port.java
+++
@@ -1,8 +0,0 @@
-package net.spy.memcached.vbucket.config;
-
-/**
- * @author alexander.sokolovsky.a@gmail.com
- */
-public enum Port {
-    direct, proxy
-}

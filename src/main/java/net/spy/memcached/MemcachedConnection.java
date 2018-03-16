7553962042f2ec9c7b41ac605234fbf0645777d1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:29 +0000

    First commited as before_28025_rev13

--- src/main/java/net/spy/memcached/MemcachedConnection.java
+++ src/main/java/net/spy/memcached/MemcachedConnection.java
@@ -49,7 +49,6 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import net.spy.memcached.compat.SpyThread;
@@ -132,7 +131,6 @@ public class MemcachedConnection extends SpyThread {
   private final Collection<Operation> retryOps;
   protected final ConcurrentLinkedQueue<MemcachedNode> nodesToShutdown;
   private final boolean verifyAliveOnConnect;
-  private final ExecutorService listenerExecutorService;
 
   protected final MetricCollector metrics;
   protected final MetricType metricType;
@@ -160,7 +158,6 @@ public class MemcachedConnection extends SpyThread {
     selector = Selector.open();
     retryOps = new ArrayList<Operation>();
     nodesToShutdown = new ConcurrentLinkedQueue<MemcachedNode>();
-    listenerExecutorService = f.getListenerExecutorService();
     this.bufSize = bufSize;
     this.connectionFactory = f;
 
@@ -488,8 +485,7 @@ public class MemcachedConnection extends SpyThread {
             // Test to see if it's truly alive, could be a hung process, OS
             final CountDownLatch latch = new CountDownLatch(1);
             final OperationFuture<Boolean> rv =
-              new OperationFuture<Boolean>("noop", latch, 2500,
-                listenerExecutorService);
+              new OperationFuture<Boolean>("noop", latch, 2500);
             NoopOperation testOp = opFact.noop(new OperationCallback() {
               public void receivedStatus(OperationStatus status) {
                 rv.set(status.isSuccess(), status);

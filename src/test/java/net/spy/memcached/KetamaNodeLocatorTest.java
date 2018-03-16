5303c9cd6abbd2a70a0b6c63dbdbe5987db22d92
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:50:18 +0000

    First commited as before_47624_rev4

--- src/test/java/net/spy/memcached/KetamaNodeLocatorTest.java
+++ src/test/java/net/spy/memcached/KetamaNodeLocatorTest.java
@@ -24,9 +24,10 @@
 package net.spy.memcached;
 
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import org.jmock.Mock;
 
 /**
@@ -181,28 +182,6 @@ public class KetamaNodeLocatorTest extends AbstractNodeLocationCase {
 
     }
     return nodes;
-  }
-
-
-  public void testLibKetamaWeightedCompatLibmemcached() {
-      String[] servers = {"127.0.0.1:11211", "127.0.0.1:11212"};
-      List<MemcachedNode> nodes = Arrays.asList(mockNodes(servers));
-      Map<InetSocketAddress, Integer> weights = new HashMap<InetSocketAddress, Integer>();
-      weights.put((InetSocketAddress) nodes.get(0).getSocketAddress(), 8);
-      weights.put((InetSocketAddress) nodes.get(1).getSocketAddress(), 8);
-
-      locator = new KetamaNodeLocator(nodes, DefaultHashAlgorithm.KETAMA_HASH,
-              KetamaNodeKeyFormatter.Format.LIBMEMCACHED, weights);
-
-      String[][] exp = { { "key1", "localhost/127.0.0.1:11212" }, { "key2", "localhost/127.0.0.1:11211" },
-              { "key3", "localhost/127.0.0.1:11212" }, { "key4", "localhost/127.0.0.1:11212" } };
-
-      for (String[] s : exp) {
-          String k = s[0];
-          String server = s[1];
-          MemcachedNode n = locator.getPrimary(k);
-          assertEquals(server, n.getSocketAddress().toString());
-      }
   }
 
   public void testLibKetamaCompatTwo() {

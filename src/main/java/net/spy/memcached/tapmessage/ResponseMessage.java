961346eb94669ce1f4ae19d3b7cf135c096b586f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:07 +0000

    First commited as before_13996_rev3

--- src/main/java/net/spy/memcached/tapmessage/ResponseMessage.java
+++ src/main/java/net/spy/memcached/tapmessage/ResponseMessage.java
@@ -235,14 +235,7 @@ public class ResponseMessage extends BaseMessage {
   }
 
   public ByteBuffer getBytes() {
-    int bufSize = 0;
-    bufSize += HEADER_LENGTH;
-    if (opcode.equals(TapOpcode.MUTATION)) {
-      bufSize += 16;
-    }
-    bufSize += getTotalbody();
-
-    ByteBuffer bb = ByteBuffer.allocate(bufSize);
+    ByteBuffer bb = ByteBuffer.allocate(HEADER_LENGTH + getTotalbody());
     bb.put(magic.getMagic());
     bb.put(opcode.getOpcode());
     bb.putShort(keylength);

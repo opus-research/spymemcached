4c5fa60a969a9cc69f533835cdff4361319cc8e3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:48:52 +0000

    First commited as after_11112_rev2

---
+++ src/main/java/net/spy/memcached/protocol/binary/UnobserveOperationImpl.java
@@ -0,0 +1,29 @@
+package net.spy.memcached.protocol.binary;
+
+import net.spy.memcached.ops.KeyedOperation;
+import net.spy.memcached.ops.OperationCallback;
+
+public class UnobserveOperationImpl extends SingleKeyOperationImpl
+  implements KeyedOperation {
+
+  static final byte UNOBS_CMD = (byte) 0xb2;
+
+  /**
+   * Length of the extra header stuff for a Unobserve response.
+   */
+  static final int EXTRA_HDR_LEN = 0;
+
+  private final long cas;
+  private final String obsSet;
+
+  public UnobserveOperationImpl(String key, long c, String os, OperationCallback cb) {
+    super(UNOBS_CMD, generateOpaque(), key, cb);
+    cas = c;
+    obsSet = os;
+  }
+
+  @Override
+  public void initialize() {
+    prepareBuffer(key, cas, obsSet.getBytes());
+  }
+}

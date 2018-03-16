221a6c81cca888d2d4eca6f566cfa5faea83e624
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:46 +0000

    First commited as after_6873_rev3

--- src/main/java/net/spy/memcached/protocol/binary/BinaryOperationFactory.java
+++ src/main/java/net/spy/memcached/protocol/binary/BinaryOperationFactory.java
@@ -2,6 +2,7 @@ package net.spy.memcached.protocol.binary;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Map;
 
 import javax.security.auth.callback.CallbackHandler;
@@ -30,8 +31,11 @@ import net.spy.memcached.ops.SASLStepOperation;
 import net.spy.memcached.ops.StatsOperation;
 import net.spy.memcached.ops.StoreOperation;
 import net.spy.memcached.ops.StoreType;
+import net.spy.memcached.ops.TapOperation;
 import net.spy.memcached.ops.VersionOperation;
 import net.spy.memcached.ops.GetOperation.Callback;
+import net.spy.memcached.tapmessage.Opcode;
+import net.spy.memcached.tapmessage.RequestMessage;
 
 /**
  * Factory for binary operations.
@@ -140,4 +144,17 @@ public class BinaryOperationFactory extends BaseOperationFactory {
 				props, cbh, cb);
 	}
 
+	public TapOperation tapBackfill(String id, Date date, String keyFilter,
+			String valueFilter, OperationCallback cb) {
+		return new TapBackfillOperationImpl(id, date, keyFilter, valueFilter, cb);
+	}
+
+	public TapOperation tapCustom(String id, RequestMessage message, String keyFilter,
+			String valueFilter, OperationCallback cb) {
+		return new TapCustomOperationImpl(id, message, keyFilter, valueFilter, cb);
+	}
+
+	public TapOperation tapAck(Opcode opcode, int opaque, OperationCallback cb) {
+		return new TapAckOperationImpl(opcode, opaque, cb);
+	}
 }
---
+++ src/main/java/net/spy/memcached/protocol/binary/TapCustomOperationImpl.java
@@ -0,0 +1,41 @@
+package net.spy.memcached.protocol.binary;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import net.spy.memcached.ops.OperationCallback;
+import net.spy.memcached.ops.OperationState;
+import net.spy.memcached.ops.TapOperation;
+import net.spy.memcached.tapmessage.RequestMessage;
+
+public class TapCustomOperationImpl extends TapOperationImpl implements TapOperation {
+	private final String id;
+	private final RequestMessage message;
+
+	TapCustomOperationImpl(String id, RequestMessage message, String keyFilter,
+			String valueFilter, OperationCallback cb) {
+		super(cb, keyFilter, valueFilter);
+		this.id = id;
+		this.message = message;
+	}
+
+	@Override
+	public void initialize() {
+		if (id != null) {
+			message.setName(id);
+		} else {
+			message.setName(UUID.randomUUID().toString());
+		}
+		setBuffer(message.getBytes());
+	}
+
+	@Override
+	public Collection<String> getKeys() {
+		return null;
+	}
+
+	@Override
+	public void streamClosed(OperationState state) {
+		transitionState(state);
+	}
+}

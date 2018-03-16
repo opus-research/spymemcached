723de42dc10bfec66c262bcfe169d8e8c0862cf7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:50:11 +0000

    First commited as before_38358_rev1

--- src/main/java/net/spy/memcached/compat/log/DefaultLogger.java
+++ src/main/java/net/spy/memcached/compat/log/DefaultLogger.java
@@ -73,7 +73,8 @@ public class DefaultLogger extends AbstractLogger {
    */
   @Override
   public synchronized void log(Level level, Object message, Throwable e) {
-    if (level == Level.WARN
+    if (level == Level.INFO
+        || level == Level.WARN
         || level == Level.ERROR
         || level == Level.FATAL) {
       System.err.printf("%s %s %s:  %s\n", df.format(new Date()), level.name(),

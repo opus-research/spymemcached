28104e7d943c51cb2ddcf20251f1b3119d299f07
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:12 +0000

    First commited as before_16335_rev3

--- src/main/java/net/spy/memcached/MemcachedConnection.java
+++ src/main/java/net/spy/memcached/MemcachedConnection.java
@@ -468,7 +468,7 @@ public class MemcachedConnection extends SpyThread {
             assert op == currentOp : "Expected to pop " + currentOp + " got "
                 + op;
           } else if (currentOp.getState() == OperationState.RETRY) {
-            getLogger().warn("Reschedule read op due to NOT_MY_VBUCKET error: "
+            getLogger().debug("Reschedule read op due to NOT_MY_VBUCKET error: "
                 + "%s ", currentOp);
             ((VBucketAware) currentOp).addNotMyVbucketNode(
                 currentOp.getHandlingNode());
@@ -501,7 +501,7 @@ public class MemcachedConnection extends SpyThread {
     return sb.toString();
   }
 
-  protected void queueReconnect(MemcachedNode qa) {
+  private void queueReconnect(MemcachedNode qa) {
     if (!shutDown) {
       getLogger().warn("Closing, and reopening %s, attempt %d.", qa,
           qa.getReconnectCount());

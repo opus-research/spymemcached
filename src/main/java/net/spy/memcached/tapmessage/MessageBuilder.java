c50db5ede81bff46ab23e329b43e76caa8ce2d73
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:48:34 +0000

    First commited as before_9712_rev6

--- src/main/java/net/spy/memcached/tapmessage/MessageBuilder.java
+++ src/main/java/net/spy/memcached/tapmessage/MessageBuilder.java
@@ -18,7 +18,7 @@ public class MessageBuilder {
 		message.setFlags(TapFlag.DUMP);
 	}
 
-	public void specifyVbuckets(short[] vbucketlist) {
+	public void specifyVbuckets(int[] vbucketlist) {
 		message.setVbucketlist(vbucketlist);
 		message.setFlags(TapFlag.LIST_VBUCKETS);
 	}
@@ -31,7 +31,7 @@ public class MessageBuilder {
 		message.setFlags(TapFlag.KEYS_ONLY);
 	}
 
-	public void takeoverVbuckets(short[] vbucketlist) {
+	public void takeoverVbuckets(int[] vbucketlist) {
 		message.setVbucketlist(vbucketlist);
 		message.setFlags(TapFlag.TAKEOVER_VBUCKETS);
 	}

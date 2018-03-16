47ac1a52151d41e6f149f5971454d4dc4762b493
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:27 +0000

    First commited as after_6672_rev4

---
+++ src/test/java/net/spy/memcached/vbucket/config/ConfigurationParserMock.java
@@ -0,0 +1,124 @@
+package net.spy.memcached.vbucket.config;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.spy.memcached.HashAlgorithm;
+
+public class ConfigurationParserMock implements ConfigurationParser {
+    private boolean parseBaseCalled = false;
+    private boolean parseBucketsCalled = false;
+    private boolean parseBucketCalled = false;
+    private boolean loadPoolCalled = false;
+    private String poolName = "default";
+    private String poolUri = "/pools/default";
+    private String poolStreamingUri = "/poolsStreaming/default";
+    private String bucketName = "Administrator";
+    private DefaultConfig vbuckets = new DefaultConfig(HashAlgorithm.NATIVE_HASH, 1, 1, 1,
+            Collections.<String>emptyList(), Collections.<VBucket>emptyList());
+    private String bucketsUri = "/pools/default/buckets";
+    private String bucketStreamingUri = "/pools/default/bucketsStreaming/Administrator";
+    private List<Node> nodes = Collections.singletonList(
+            new Node(Status.healthy, "localhost", Collections.singletonMap(Port.direct, "11210")));
+
+    @Override
+    public Map<String, Pool> parseBase(String base) throws ParseException {
+        Map<String, Pool> result = new HashMap<String, Pool>();
+        try {
+            parseBaseCalled = true;
+            Pool pool = new Pool(poolName, new URI(poolUri), new URI(poolStreamingUri));
+            result.put(poolName, pool);
+        } catch (URISyntaxException e) {
+            throw new ParseException(e.getMessage(), 0);
+        }
+        return result;
+    }
+
+    @Override
+    public Map<String, Bucket> parseBuckets(String buckets) throws ParseException {
+        Map<String, Bucket> result = new HashMap<String, Bucket>();
+        try {
+            parseBucketsCalled = true;
+            Bucket bucket = new Bucket(bucketName, vbuckets, new URI(bucketStreamingUri), nodes);
+            result.put(bucketName, bucket);
+        } catch (URISyntaxException e) {
+            throw new ParseException(e.getMessage(), 0);
+        }
+        return result;
+    }
+
+    @Override
+    public Bucket parseBucket(String sBucket) throws ParseException {
+        parseBucketCalled = true;
+        try {
+            parseBucketsCalled = true;
+            return new Bucket(bucketName, vbuckets, new URI(bucketStreamingUri), nodes);
+        } catch (URISyntaxException e) {
+            throw new ParseException(e.getMessage(), 0);
+        }
+
+    }
+
+    @Override
+    public void loadPool(Pool pool, String sPool) throws ParseException {
+        try {
+            loadPoolCalled = true;
+            pool.setBucketsUri(new URI(bucketsUri));
+        } catch (URISyntaxException e) {
+            throw new ParseException(e.getMessage(), 0);
+        }
+    }
+
+    public boolean isParseBaseCalled() {
+        return parseBaseCalled;
+    }
+
+    public boolean isParseBucketsCalled() {
+        return parseBucketsCalled;
+    }
+
+    public boolean isParseBucketCalled() {
+        return parseBucketCalled;
+    }
+
+    public boolean isLoadPoolCalled() {
+        return loadPoolCalled;
+    }
+
+    public void setPoolName(String poolName) {
+        this.poolName = poolName;
+    }
+
+    public void setPoolUri(String poolUri) {
+        this.poolUri = poolUri;
+    }
+
+    public void setPoolStreamingUri(String poolStreamingUri) {
+        this.poolStreamingUri = poolStreamingUri;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public void setVbuckets(DefaultConfig vbuckets) {
+        this.vbuckets = vbuckets;
+    }
+
+    public void setBucketsUri(String bucketsUri) {
+        this.bucketsUri = bucketsUri;
+    }
+
+    public void setBucketStreamingUri(String bucketStreamingUri) {
+        this.bucketStreamingUri = bucketStreamingUri;
+    }
+
+    public void setNodes(List<Node> nodes) {
+        this.nodes = nodes;
+    }
+}

e2fa0d36bc21a75ffebff602b7ef9b8afef7d540
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:49:07 +0000

    First commited as before_14142_rev1

--- src/main/java/net/spy/memcached/tapmessage/ResponseMessage.java
+++ src/main/java/net/spy/memcached/tapmessage/ResponseMessage.java
@@ -80,12 +80,7 @@ public class ResponseMessage extends BaseMessage {
     }
 
     if (opcode.equals(TapOpcode.MUTATION)) {
-      if (flags.contains(TapResponseFlag.TAP_FLAG_NETWORK_BYTE_ORDER)) {
-        itemflags = decodeInt(b, ITEM_FLAGS_OFFSET);
-      } else {
-        // handles Couchbase bug MB-4834
-        itemflags = decodeIntHostOrder(b, ITEM_FLAGS_OFFSET);
-      }
+      itemflags = decodeInt(b, ITEM_FLAGS_OFFSET);
       itemexpiry = decodeInt(b, ITEM_EXPIRY_OFFSET);
       vbucketstate = 0;
       revid = new byte[engineprivate];
@@ -235,14 +230,7 @@ public class ResponseMessage extends BaseMessage {
   }
 
   public ByteBuffer getBytes() {
-    int bufSize = 0;
-    bufSize += HEADER_LENGTH;
-    if (opcode.equals(TapOpcode.MUTATION)) {
-      bufSize += 16;
-    }
-    bufSize += getTotalbody();
-
-    ByteBuffer bb = ByteBuffer.allocate(bufSize);
+    ByteBuffer bb = ByteBuffer.allocate(HEADER_LENGTH + getTotalbody());
     bb.put(magic.getMagic());
     bb.put(opcode.getOpcode());
     bb.putShort(keylength);
@@ -261,7 +249,7 @@ public class ResponseMessage extends BaseMessage {
 
     short flag = 0;
     for (int i = 0; i < flags.size(); i++) {
-      flag |= flags.get(i).getFlags();
+      flag |= flags.get(i).getFlag();
     }
 
     bb.putShort(flag);

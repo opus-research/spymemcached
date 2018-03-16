76fdbb7e5d8181ed82f5358b8ed0cb1f594bdda5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:59 +0000

    First commited as after_7650_rev6

--- src/test/java/net/spy/memcached/BinaryClientTest.java
+++ src/test/java/net/spy/memcached/BinaryClientTest.java
@@ -2,7 +2,9 @@ package net.spy.memcached;
 
 
 /**
- * This test assumes a binary server is running on localhost:11211.
+ * This test assumes a binary server is running on the host specified
+ * int the environment variable SPYMC_TEST_SERVER_V4 or
+ * localhost:11211 by default.
  */
 public class BinaryClientTest extends ProtocolBaseCase {
 
@@ -22,7 +24,7 @@ public class BinaryClientTest extends ProtocolBaseCase {
 
 	@Override
 	protected String getExpectedVersionSource() {
-		return "/127.0.0.1:11211";
+		return "/" + TestConfig.IPV4_ADDR + ":11211";
 	}
 
 	@Override
--- src/test/java/net/spy/memcached/BinaryIPV6ClientTest.java
+++ src/test/java/net/spy/memcached/BinaryIPV6ClientTest.java
@@ -8,12 +8,16 @@ public class BinaryIPV6ClientTest extends BinaryClientTest {
 	@Override
 	protected void initClient(ConnectionFactory cf) throws Exception {
 		client=new MemcachedClient(cf,
-			AddrUtil.getAddresses("::1:11211"));
+			AddrUtil.getAddresses(TestConfig.IPV6_ADDR + ":11211"));
 	}
 
 	@Override
 	protected String getExpectedVersionSource() {
-		return "/0:0:0:0:0:0:0:1:11211";
+		// If no ipv6 address is given and we're not using localhost then we default to ipv4
+		if (TestConfig.IPV4_ADDR.equals(System.getProperty(TestConfig.IPV6_PROP, "::1"))) {
+			return "/" + TestConfig.IPV4_ADDR + ":11211";
+		}
+		return "/" + TestConfig.IPV6_ADDR + ":11211";
 	}
 
 }

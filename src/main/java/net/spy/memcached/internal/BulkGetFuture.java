01e635400be18eddd03731cd26294c2d42e2b95b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:58 +0000

    First commited as after_7648_rev1

--- src/main/java/net/spy/memcached/internal/BulkGetFuture.java
+++ src/main/java/net/spy/memcached/internal/BulkGetFuture.java
@@ -117,6 +117,7 @@ public class BulkGetFuture<T> implements BulkFuture<Map<String, T>> {
         if (!latch.await(to, unit)) {
             for (Operation op : ops) {
                 if (op.getState() != OperationState.COMPLETE) {
+                	System.out.println(op.getState());
                     MemcachedConnection.opTimedOut(op);
                     timedoutOps.add(op);
                 } else {

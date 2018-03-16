21ba80038c9af09a416886ae005feab4bc642bb1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:38 +0000

    First commited as before_6820_rev4

--- src/main/java/net/spy/memcached/AddrUtil.java
+++ src/main/java/net/spy/memcached/AddrUtil.java
@@ -51,19 +51,21 @@ public class AddrUtil {
 	}
 
 	public static List<InetSocketAddress> getAddresses(List<String> servers) {
-		ArrayList<InetSocketAddress> addrs = new ArrayList<InetSocketAddress>();
+		ArrayList<InetSocketAddress> addrs = new ArrayList<InetSocketAddress>(servers.size());
 		for (String server : servers) {
 			int finalColon=server.lastIndexOf(':');
 			if(finalColon < 1) {
 				throw new IllegalArgumentException("Invalid server ``"
 				+ server + "'' in list:  " + server);
-		}
+			}
 		String hostPart=server.substring(0, finalColon);
 		String portNum=server.substring(finalColon+1);
 
 		addrs.add(new InetSocketAddress(hostPart, Integer.parseInt(portNum)));
 		}
-		assert !addrs.isEmpty() : "No addrs found";
+		if (addrs.isEmpty()) {
+		    throw new IllegalArgumentException("servers cannot be empty"); //servers was passed in empty, and shouldn't have been
+		}
 		return addrs;
 
 	}

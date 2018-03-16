6c80efaa839b828675f600563d598d598f825471
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:19 +0000

    First commited as after_4206_rev2

--- src/main/java/net/spy/memcached/internal/OperationFuture.java
+++ src/main/java/net/spy/memcached/internal/OperationFuture.java
@@ -74,13 +74,9 @@ public class OperationFuture<T> implements Future<T> {
 		if(isCancelled()) {
 			throw new ExecutionException(new RuntimeException("Cancelled"));
 		}
-<<<<<<< HEAD
-                if(op.isTimedOut()) {
-                        throw new ExecutionException(new RuntimeException("Operation timed out."));
-=======
+
                 if(op != null && op.isTimedOut()) {
                         throw new ExecutionException(new CheckedOperationTimeoutException("Operation timed out.", op));
->>>>>>> b4f449f... sq Do not write timedout operations part 3
                 }
 
 		return objRef.get();

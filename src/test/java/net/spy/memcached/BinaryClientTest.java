a9c6dd41e06c4fa15cd4843f4196e13777c8d5a5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:49 +0000

    First commited as after_6906_rev2

--- src/test/java/net/spy/memcached/BinaryClientTest.java
+++ src/test/java/net/spy/memcached/BinaryClientTest.java
@@ -62,4 +62,16 @@ public class BinaryClientTest extends ProtocolBaseCase {
 		assertTrue(client.prepend(casv.getCas(), key, "es").get());
 		assertEquals("estest", client.get(key));
 	}
+
+	public void testTouchTimeout() throws Exception {
+		if (isMembase()) {
+			assertNull(client.get("touchkey"));
+			assert client.set("touchkey", 1, "touchvalue").get().booleanValue();
+			assert client.touch("touchkey", 2).get().booleanValue();
+			Thread.sleep(1300);
+			assert client.get("touchkey").equals("touchvalue");
+			Thread.sleep(2000);
+			assertFalse(client.touch("touchkey", 3).get().booleanValue());
+		}
+	}
 }

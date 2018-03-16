e612398b33efac87966f144339460437eb5b0c5a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:58 +0000

    First commited as before_9003_rev3

--- src/main/java/net/spy/memcached/protocol/couch/GetViewOperation.java
+++
@@ -1,9 +0,0 @@
-package net.spy.memcached.protocol.couch;
-
-import net.spy.memcached.ops.OperationCallback;
-
-public interface GetViewOperation {
-	public interface GetViewCallback extends OperationCallback {
-		void gotData(View view);
-	}
-}

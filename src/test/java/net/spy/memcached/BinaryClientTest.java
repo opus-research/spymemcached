6bf90d528163b64d23dad7800ca92a7f6d057d0f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:46:53 +0000

    First commited as before_7048_rev3

--- src/test/java/net/spy/memcached/BinaryClientTest.java
+++ src/test/java/net/spy/memcached/BinaryClientTest.java
@@ -1,13 +1,5 @@
 package net.spy.memcached;
 
-import java.net.URI;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
-
-import net.spy.memcached.internal.SyncRequest;
-import net.spy.memcached.internal.SyncResponse;
 
 /**
  * This test assumes a binary server is running on localhost:11211.
@@ -21,7 +13,6 @@ public class BinaryClientTest extends ProtocolBaseCase {
 			public long getOperationTimeout() {
 				return 15000;
 			}
-
 			@Override
 			public FailureMode getFailureMode() {
 				return FailureMode.Retry;
@@ -36,12 +27,12 @@ public class BinaryClientTest extends ProtocolBaseCase {
 
 	@Override
 	public void testGetStatsCacheDump() throws Exception {
-		// XXX: Cachedump isn't returning anything from the server in binprot
+		// XXX:  Cachedump isn't returning anything from the server in binprot
 		assertTrue(true);
 	}
 
 	public void testCASAppendFail() throws Exception {
-		final String key = "append.key";
+		final String key="append.key";
 		assertTrue(client.set(key, 5, "test").get());
 		CASValue<Object> casv = client.gets(key);
 		assertFalse(client.append(casv.getCas() + 1, key, "es").get());
@@ -49,7 +40,7 @@ public class BinaryClientTest extends ProtocolBaseCase {
 	}
 
 	public void testCASAppendSuccess() throws Exception {
-		final String key = "append.key";
+		final String key="append.key";
 		assertTrue(client.set(key, 5, "test").get());
 		CASValue<Object> casv = client.gets(key);
 		assertTrue(client.append(casv.getCas(), key, "es").get());
@@ -57,7 +48,7 @@ public class BinaryClientTest extends ProtocolBaseCase {
 	}
 
 	public void testCASPrependFail() throws Exception {
-		final String key = "append.key";
+		final String key="append.key";
 		assertTrue(client.set(key, 5, "test").get());
 		CASValue<Object> casv = client.gets(key);
 		assertFalse(client.prepend(casv.getCas() + 1, key, "es").get());
@@ -65,42 +56,10 @@ public class BinaryClientTest extends ProtocolBaseCase {
 	}
 
 	public void testCASPrependSuccess() throws Exception {
-		final String key = "append.key";
+		final String key="append.key";
 		assertTrue(client.set(key, 5, "test").get());
 		CASValue<Object> casv = client.gets(key);
 		assertTrue(client.prepend(casv.getCas(), key, "es").get());
 		assertEquals("estest", client.get(key));
-	}
-
-	public void testSync() throws Exception {
-		if (isMembase()) {
-			client = new MemcachedClient(Arrays.asList(new URI(
-					"http://localhost:8091/pools")), "default", "default", "");
-			Collection<SyncRequest> keys = new LinkedList<SyncRequest>();
-
-			for (int i = 0; i < 20; i++) {
-				keys.add(new SyncRequest("key" + i));
-			}
-
-			for (int i = 0; i < 10; i++) {
-				client.set("key" + i, 0, "value" + i);
-			}
-
-			Collection<SyncResponse> resp = client.asyncSync(keys, 0, false,
-					false, false).get();
-			Iterator<SyncResponse> itr = resp.iterator();
-			int passed = 0;
-			int failed = 0;
-			while (itr.hasNext()) {
-				SyncResponse cur = itr.next();
-				if (cur.getStatus().isSuccess()) {
-					passed++;
-				} else {
-					failed++;
-				}
-			}
-			System.out.println(passed + " " + failed);
-			client.flush();
-		}
 	}
 }

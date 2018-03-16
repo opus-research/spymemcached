cf663c3af3851811ce911d7c10191d23da7739dd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:50:09 +0000

    First commited as before_37724_rev2

--- src/main/java/net/spy/memcached/MemcachedConnection.java
+++ src/main/java/net/spy/memcached/MemcachedConnection.java
@@ -389,7 +389,7 @@ public class MemcachedConnection extends SpyThread {
    */
   public void handleIO() throws IOException {
     if (shutDown) {
-      return;
+      throw new IOException("No IO while shut down");
     }
 
     handleInputQueue();
@@ -1285,26 +1285,23 @@ public class MemcachedConnection extends SpyThread {
    */
   public void shutdown() throws IOException {
     shutDown = true;
-    try {
-      Selector s = selector.wakeup();
-      assert s == selector : "Wakeup returned the wrong selector.";
-      for (MemcachedNode node : locator.getAll()) {
-        if (node.getChannel() != null) {
-          node.getChannel().close();
-          node.setSk(null);
-          if (node.getBytesRemainingToWrite() > 0) {
-            getLogger().warn("Shut down with %d bytes remaining to write",
-              node.getBytesRemainingToWrite());
-          }
-          getLogger().debug("Shut down channel %s", node.getChannel());
-        }
-      }
 
-      selector.close();
-      getLogger().debug("Shut down selector %s", selector);
-    } finally {
-      running = false;
+    Selector s = selector.wakeup();
+    assert s == selector : "Wakeup returned the wrong selector.";
+    for (MemcachedNode node : locator.getAll()) {
+      if (node.getChannel() != null) {
+        node.getChannel().close();
+        node.setSk(null);
+        if (node.getBytesRemainingToWrite() > 0) {
+          getLogger().warn("Shut down with %d bytes remaining to write",
+              node.getBytesRemainingToWrite());
+        }
+        getLogger().debug("Shut down channel %s", node.getChannel());
+      }
     }
+    running = false;
+    selector.close();
+    getLogger().debug("Shut down selector %s", selector);
   }
 
   @Override

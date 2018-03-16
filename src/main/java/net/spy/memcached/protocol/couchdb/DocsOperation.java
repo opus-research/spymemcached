eb6dd56da1567041859ecd61f5dcad7dceb13eec
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:18 +0000

    First commited as before_8401_rev7

--- src/main/java/net/spy/memcached/protocol/couchdb/DocsOperation.java
+++
@@ -1,9 +0,0 @@
-package net.spy.memcached.protocol.couchdb;
-
-import net.spy.memcached.ops.OperationCallback;
-
-public interface DocsOperation {
-	interface DocsCallback extends OperationCallback {
-		void gotData(ViewResponseWithDocs response);
-	}
-}

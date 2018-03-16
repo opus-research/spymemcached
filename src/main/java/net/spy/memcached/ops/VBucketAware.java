e53082e2691575c25aade7a550e577d7976e05e5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:03 +0000

    First commited as before_7801_rev4

--- src/main/java/net/spy/memcached/ops/VBucketAware.java
+++ src/main/java/net/spy/memcached/ops/VBucketAware.java
@@ -4,13 +4,8 @@ import net.spy.memcached.MemcachedNode;
 
 import java.util.Collection;
 
-/**
- * Operations that depend on a VBucket number being sent to the server
- * are required to implement this interface.
- */
 public interface VBucketAware {
-    void setVBucket(String key, short vbucket);
-    short getVBucket(String key);
+    void setVBucket(int vbucket);
     Collection<MemcachedNode> getNotMyVbucketNodes();
     void addNotMyVbucketNode(MemcachedNode node);
     void setNotMyVbucketNodes(Collection<MemcachedNode> nodes);

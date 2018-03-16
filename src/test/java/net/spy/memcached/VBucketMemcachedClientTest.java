c05751b0bfd339b1f7e912d7f24c5b8d6fbf9987
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:47:01 +0000

    First commited as before_7672_rev1

--- src/test/java/net/spy/memcached/VBucketMemcachedClientTest.java
+++ src/test/java/net/spy/memcached/VBucketMemcachedClientTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import net.spy.memcached.vbucket.ConfigurationException;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Arrays;
@@ -31,7 +32,7 @@ public class VBucketMemcachedClientTest extends TestCase {
 
         Integer i;
         for (i = 0; i < 10000; i++) {
-            mc.set("test" + i, 0, i.toString());
+            Future result = mc.set("test" + i, 0, i.toString());
         }
         mc.set("hello", 0, "world");
         String result = (String) mc.get("hello");
